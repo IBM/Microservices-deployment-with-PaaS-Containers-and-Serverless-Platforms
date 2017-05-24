a756470521f6bb4f421490da87e7ad6ac9d19195
Tommy Li <Tommy.chaoping.li@ibm.com>
2017-05-24 09:27:02 -0700

    updated script for openwhisk

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,25 +23,26 @@ kubectl delete --ignore-not-found=true -f secret.yaml
 bx cs cluster-service-bind $CLUSTER_NAME default mycloudant
 bx cs cluster-service-bind $CLUSTER_NAME default myweatherinsights
 
-sed -i s#<insert-app-ID>#$FLIGHTSTATS_APP_ID# secret.yaml
-sed -i s#<insert-app-key>#$FLIGHTSTATS_APP_KEY# secret.yaml
-sed -i s#<insert-API-key>#$TRIPIT_API_KEY# secret.yaml
-sed -i s#<insert-API-secret>#$TRIPIT_API_SECRET# secret.yaml
+sed -i s#"<insert-app-ID>"#$FLIGHTSTATS_APP_ID# secret.yaml
+sed -i s#"<insert-app-key>"#$FLIGHTSTATS_APP_KEY# secret.yaml
+sed -i s#"<insert-API-key>"#$TRIPIT_API_KEY# secret.yaml
+sed -i s#"<insert-API-secret>"#$TRIPIT_API_SECRET# secret.yaml
 
 kubectl create -f secret.yaml
 
-if [ -z "$OPENWHISK_AUTH"]; then
-	sed -i s#<your-app-end-point-url>#$IP_ADDR:30080# flightassist.yaml
-	sed -i s#registry.ng.bluemix.net/<namespace>/flightassist#docker.io/tomcli/flightassist# flightassist.yaml
-	sed -i s#registry.ng.bluemix.net/<namespace>/weather-service#docker.io/tomcli/weather-service# flightassist.yaml
+if [ -z $OPENWHISK_AUTH ]; then
+	sed -i s#"<your-app-end-point-url>"#$IP_ADDR:30080# flightassist.yaml
+	sed -i s#"registry.ng.bluemix.net/<namespace>/flightassist"#docker.io/tomcli/flightassist# flightassist.yaml
+	sed -i s#"registry.ng.bluemix.net/<namespace>/weather-service"#docker.io/tomcli/weather-service# flightassist.yaml
 
 	kubectl create -f flightassist.yaml
 else
-	sed -i s#<your-app-end-point-url>#$IP_ADDR:30080# flightassist_serverless.yaml
-	sed -i s#registry.ng.bluemix.net/<namespace>/flightassist#docker.io/tomcli/flightassist# flightassist_serverless.yaml
-	sed -i s#<your-openwhisk-auth>#$OPENWHISK_AUTH# flightassist_serverless.yaml
+	sed -i s#"<your-app-end-point-url>"#$IP_ADDR:30080# flightassist_serverless.yaml
+	sed -i s#"registry.ng.bluemix.net/<namespace>/flightassist"#docker.io/tomcli/flightassist# flightassist_serverless.yaml
+	sed -i s#"<your-openwhisk-auth>"#$OPENWHISK_AUTH# flightassist_serverless.yaml
 
 	kubectl create -f flightassist_serverless.yaml
 fi
 
 echo "" && echo "View your FlightAssist website at http://$IP_ADDR:30080"
+
